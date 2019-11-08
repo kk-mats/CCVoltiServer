21a7209af9ba259d0656cee5fd5a9266f00a1832
kk-mats <k.mats.kk@gmail.com>
2019-11-09 00:26:27 +0900

    feat: Add Label.priority for arguments ordering

--- src/main/kotlin/io/github/kk_mats/ccvoltiserver/domain/type/Label.kt
+++ src/main/kotlin/io/github/kk_mats/ccvoltiserver/domain/type/Label.kt
@@ -1,6 +1,23 @@
 package io.github.kk_mats.ccvoltiserver.domain.type
 
-data class Label(val name: String, val slug: String = name) : Comparable<Label> {
+fun toSlug(str: String): String {
+	return str.replace(Regex("^-+"), "")
+}
+
+class Label(
+		val option: String,
+		val name: String = option,
+		val slug: String = toSlug(option),
+		val priority: Int = 0
+) : Comparable<Label> {
+	constructor(option: String, priority: Int) : this(option, option, toSlug(option), priority)
+	constructor(option: String, name: String = toSlug(option), priority: Int) : this(
+			option,
+			name,
+			toSlug(option),
+			priority
+	)
+
 	override fun equals(other: Any?): Boolean {
 		return when {
 			this === other -> true
@@ -16,5 +33,9 @@ data class Label(val name: String, val slug: String = name) : Comparable<Label> 
 
 	override fun compareTo(other: Label): Int {
 		return this.slug.compareTo(other.slug)
+	}
+
+	override fun toString(): String {
+		return this.slug
 	}
 }
