1deb5bf34e23117d6d67194b5c5696b71e7e1350
kk-mats <k.mats.kk@gmail.com>
2019-11-09 00:31:13 +0900

    feat: Add limitations of Int, Double and Regex

--- src/main/kotlin/io/github/kk_mats/ccvoltiserver/domain/repository/DetectorInfoRepository.kt
+++ src/main/kotlin/io/github/kk_mats/ccvoltiserver/domain/repository/DetectorInfoRepository.kt
@@ -1,12 +1,15 @@
 package io.github.kk_mats.ccvoltiserver.domain.repository
 
+import io.github.kk_mats.ccvoltiserver.domain.type.Failable
 import io.github.kk_mats.ccvoltiserver.domain.type.Label
-import io.github.kk_mats.ccvoltiserver.domain.type.limitation.RangeLimitation
-import io.github.kk_mats.ccvoltiserver.domain.type.limitation.VariantLimitation
+import io.github.kk_mats.ccvoltiserver.domain.type.limitation.*
 
 interface DetectorInfoRepository {
 	fun isSupportedVersion(slug: String): Boolean
+	fun version(): VariantLimitation<Label>
 
-	fun rangeLimitations(): HashSet<RangeLimitation>
+	fun intRangeLimitations(): HashSet<RangeLimitation<Int>>
+	fun doubleRangeLimitations(): HashSet<RangeLimitation<Double>>
+	fun regexLimitations(): HashSet<RegexLimitation>
 	fun variantLimitations(): HashSet<VariantLimitation<String>>
 }
--- src/main/kotlin/io/github/kk_mats/ccvoltiserver/infrastructure/DetectorRepositoryImpl.kt
+++ src/main/kotlin/io/github/kk_mats/ccvoltiserver/infrastructure/DetectorRepositoryImpl.kt
@@ -3,8 +3,7 @@ package io.github.kk_mats.ccvoltiserver.infrastructure
 import io.github.kk_mats.ccvoltiserver.domain.`object`.DetectorInfo
 import io.github.kk_mats.ccvoltiserver.domain.repository.DetectorInfoRepository
 import io.github.kk_mats.ccvoltiserver.domain.type.Label
-import io.github.kk_mats.ccvoltiserver.domain.type.limitation.RangeLimitation
-import io.github.kk_mats.ccvoltiserver.domain.type.limitation.VariantLimitation
+import io.github.kk_mats.ccvoltiserver.domain.type.limitation.*
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -13,8 +12,20 @@ class DetectorRepositoryImpl : DetectorInfoRepository {
 		return DetectorInfo.versions.validate(Label(slug)).value != null
 	}
 
-	override fun rangeLimitations(): HashSet<RangeLimitation> {
-		return DetectorInfo.rangeLimitations
+	override fun version(): VariantLimitation<Label> {
+		return DetectorInfo.versions
+	}
+
+	override fun intRangeLimitations(): HashSet<RangeLimitation<Int>> {
+		return DetectorInfo.intRangeLimitations
+	}
+
+	override fun doubleRangeLimitations(): HashSet<RangeLimitation<Double>> {
+		return DetectorInfo.doubleRangeLimitations
+	}
+
+	override fun regexLimitations(): HashSet<RegexLimitation> {
+		return DetectorInfo.regexLimitations
 	}
 
 	override fun variantLimitations(): HashSet<VariantLimitation<String>> {
